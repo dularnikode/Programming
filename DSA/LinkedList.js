3a0e62aa0ca39c1119447386266dabe6528017a2
Dular Nikode <dularnikode1@gmail.com>
2025-05-12 23:45:14 +0530

    linked-list-inset-first-insert-last

---
+++ DSA/LinkedList.js
@@ -0,0 +1,49 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+let head = new Node(1);
+const middle = new Node(2);
+const tail = new Node(3);
+
+// Link the nodes
+head.next = middle;
+middle.next = tail;
+// Print the linked list
+
+function printLinkedList (head){
+    let temp = head;
+    while(temp !== null){
+        console.log(temp.data);
+        temp = temp.next;
+    }
+}
+
+function insertAtstartOfLinkedList (val){
+    let newNode = new Node(val);
+    newNode.next = head;
+    head = newNode;
+}
+
+function insertLast (val){
+    let newNode = new Node(val);
+    if(head === null){
+        head = newNode;
+    }else{
+        let lastNode = head;
+        while(lastNode.next !== null){
+            lastNode = lastNode.next;
+        }
+        lastNode.next = newNode;
+    }
+
+}
+
+
+// printLinkedList(head);
+// insertAtstartOfLinkedList(0)
+insertLast(20);
+printLinkedList(head);
